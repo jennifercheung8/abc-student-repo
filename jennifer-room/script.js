87648ef1eac16d5f0034631ec561380a7ca081cc
Jennifer Cheung <jennifercheung@Jennifers-MacBook-Pro.local>
2020-09-23 15:01:43 -0700

    mini1img

--- jennifer-room/script.js
+++ jennifer-room/script.js
@@ -1,23 +1,23 @@
-document.getElementById("project").addEventListener("mouseover", mouseOverProject);
-document.getElementById("project").addEventListener("mouseout", mouseOutProject);
+// document.getElementById("project").addEventListener("mouseover", mouseOverProject);
+// document.getElementById("project").addEventListener("mouseout", mouseOutProject);
 
-document.getElementById("mini").addEventListener("mouseover", mouseOverMini);
-document.getElementById("mini").addEventListener("mouseout", mouseOutMini);
+document.getElementById("one").addEventListener("mouseover", mouseOverMini1);
+document.getElementById("one").addEventListener("mouseout", mouseOutMini1);
 
 
-function mouseOverProject() {
-document.getElementById("preimg").style.opacity = "1"
-}
+// function mouseOverProject() {
+// document.getElementById("preimg").style.opacity = "1"
+// }
+//
+// function mouseOutProject() {
+// document.getElementById("preimg").style.opacity = "0"
+// }
 
-function mouseOutProject() {
-document.getElementById("preimg").style.opacity = "0"
-}
-
-function mouseOverMini() {
-document.getElementById("miniimg").style.opacity = "1"
+function mouseOverMini1() {
+document.getElementById("mini1").style.opacity = "1"
 console.log("hello");
 }
 
-function mouseOutMini() {
-document.getElementById("miniimg").style.opacity = "0"
+function mouseOutMini1() {
+document.getElementById("mini1").style.opacity = "0"
 }
