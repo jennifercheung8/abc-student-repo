3474a28b25bbedaed3daab1709dce4b6cc43f978
Jennifer Cheung <jennifercheung@Jennifers-MacBook-Pro.local>
2020-09-30 15:43:35 -0700

    mini2

--- jennifer-room/script.js
+++ jennifer-room/script.js
@@ -10,6 +10,18 @@ function mouseOutMini1() {
 document.getElementById("mini1").style.opacity = "0"
 }
 
+document.getElementById("two").addEventListener("mouseover", mouseOverMini2);
+document.getElementById("two").addEventListener("mouseout", mouseOutMini2);
+
+function mouseOverMini2() {
+document.getElementById("mini2").style.opacity = "1"
+console.log("hello");
+}
+
+function mouseOutMini2() {
+document.getElementById("mini2").style.opacity = "0"
+}
+
 // document.getElementById("project").addEventListener("mouseover", mouseOverProject);
 // document.getElementById("project").addEventListener("mouseout", mouseOutProject);
 
