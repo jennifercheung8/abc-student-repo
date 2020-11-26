173848abf156824a0ee3b0a11d461e2e5cfde4fb
jennifercheung <jcc867@nyu.edu>
2020-11-25 16:30:52 -0800

    mini7

--- jennifer-room/script.js
+++ jennifer-room/script.js
@@ -2,8 +2,7 @@ document.getElementById("one").addEventListener("mouseover", mouseOverMini1);
 document.getElementById("one").addEventListener("mouseout", mouseOutMini1);
 
 function mouseOverMini1() {
-document.getElementById("mini1").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini1").style.opacity = "1";
 }
 
 function mouseOutMini1() {
@@ -14,8 +13,7 @@ document.getElementById("two").addEventListener("mouseover", mouseOverMini2);
 document.getElementById("two").addEventListener("mouseout", mouseOutMini2);
 
 function mouseOverMini2() {
-document.getElementById("mini2").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini2").style.opacity = "1";
 }
 
 function mouseOutMini2() {
@@ -26,8 +24,7 @@ document.getElementById("three").addEventListener("mouseover", mouseOverMini3);
 document.getElementById("three").addEventListener("mouseout", mouseOutMini3);
 
 function mouseOverMini3() {
-document.getElementById("mini3").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini3").style.opacity = "1";
 }
 
 function mouseOutMini3() {
@@ -38,8 +35,7 @@ document.getElementById("four").addEventListener("mouseover", mouseOverMini4);
 document.getElementById("four").addEventListener("mouseout", mouseOutMini4);
 
 function mouseOverMini4() {
-document.getElementById("mini4").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini4").style.opacity = "1";
 }
 
 function mouseOutMini4() {
@@ -50,8 +46,7 @@ document.getElementById("five").addEventListener("mouseover", mouseOverMini5);
 document.getElementById("five").addEventListener("mouseout", mouseOutMini5);
 
 function mouseOverMini5() {
-document.getElementById("mini5").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini5").style.opacity = "1";
 }
 
 function mouseOutMini5() {
@@ -62,14 +57,23 @@ document.getElementById("six").addEventListener("mouseover", mouseOverMini6);
 document.getElementById("six").addEventListener("mouseout", mouseOutMini6);
 
 function mouseOverMini6() {
-document.getElementById("mini6").style.opacity = "1"
-console.log("hello");
+document.getElementById("mini6").style.opacity = "1";
 }
 
 function mouseOutMini6() {
 document.getElementById("mini6").style.opacity = "0"
 }
 
+document.getElementById("seven").addEventListener("mouseover", mouseOverMini7);
+document.getElementById("seven").addEventListener("mouseout", mouseOutMini7);
+
+function mouseOverMini7() {
+document.getElementById("mini7").style.opacity = "1";
+}
+
+function mouseOutMini7() {
+document.getElementById("mini7").style.opacity = "0"
+}
 
 
 document.getElementById("A").addEventListener("mouseover", mouseOverProjectA);
