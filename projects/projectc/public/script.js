0d4e7267841d8632447e14761872cd192ded0d6b
jennifercheung <jcc867@nyu.edu>
2020-11-29 23:36:36 -0800

    fix hour

--- projects/projectc/public/script.js
+++ projects/projectc/public/script.js
@@ -6,26 +6,26 @@ let clientHour = clientTime.getHours();
 console.log("client time: " + clientHour);
 socket.emit('clientHour', clientHour);
 
-if (clientTime > 22 || clientTime < 5){
+if (clientHour > 22 || clientHour < 5){
   document.body.style.background="linear-gradient(132deg, #182247 , #1f0a38)";
   document.body.classList.add("backgroundAnimation");
-} else if (clientTime == 5){
+} else if (clientHour == 5){
   document.body.style.background="linear-gradient(132deg, #363c6b, #3b346b)";
-} else if (clientTime == 6){
+} else if (clientHour == 6){
   document.body.style.background="linear-gradient(132deg, #d96f4c, #5f7bb3)";
-} else if (clientTime > 6 || clientTime < 9){
+} else if (clientHour > 6 || clientHour < 9){
   document.body.style.background="linear-gradient(132deg, #9ecce6, #82bee6)";
-} else if (clientTime > 8 || clientTime < 12){
+} else if (clientHour > 8 || clientHour < 12){
   document.body.style.background="linear-gradient(132deg, #68bae8, #62ade1)";
-} else if (clientTime > 11 || clientTime < 16){
+} else if (clientHour > 11 || clientHour < 16){
   document.body.style.background="linear-gradient(132deg, #289ddf, #2b8fe1)";
-} else if (clientTime == 16){
+} else if (clientHour == 16){
   document.body.style.background="linear-gradient(132deg, #679eb7, #6094d2)";
-} else if (clientTime == 17){
+} else if (clientHour == 17){
   document.body.style.background="linear-gradient(132deg, #f08b39, #6281e3)";
-} else if (clientTime > 17 || clientTime < 20){
+} else if (clientHour > 17 || clientHour < 20){
   document.body.style.background="linear-gradient(132deg, #2e5d9d, #133f72)";
-} else if (clientTime > 19 || clientTime < 23){
+} else if (clientHour > 19 || clientHour < 23){
   document.body.style.background="linear-gradient(132deg, #223667, #111e50)";
 }
 
