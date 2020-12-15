17f8236d18e3812aa4cc999434f673378d588046
jennifercheung <jcc867@nyu.edu>
2020-12-14 16:08:31 -0800

    room edits

--- jennifer-room/script.js
+++ jennifer-room/script.js
@@ -1,110 +1,154 @@
-document.getElementById("one").addEventListener("mouseover", mouseOverMini1);
-document.getElementById("one").addEventListener("mouseout", mouseOutMini1);
+let one = document.getElementById("one");
+one.style.opacity="0";
+one.addEventListener("mouseover", mouseOverMini1);
+one.addEventListener("mouseout", mouseOutMini1);
 
 function mouseOverMini1() {
 document.getElementById("mini1").style.opacity = "1";
 }
-
 function mouseOutMini1() {
 document.getElementById("mini1").style.opacity = "0"
 }
 
-document.getElementById("two").addEventListener("mouseover", mouseOverMini2);
-document.getElementById("two").addEventListener("mouseout", mouseOutMini2);
+let two = document.getElementById("two");
+two.style.opacity="0";
+two.addEventListener("mouseover", mouseOverMini2);
+two.addEventListener("mouseout", mouseOutMini2);
 
 function mouseOverMini2() {
 document.getElementById("mini2").style.opacity = "1";
 }
-
 function mouseOutMini2() {
 document.getElementById("mini2").style.opacity = "0"
 }
 
-document.getElementById("three").addEventListener("mouseover", mouseOverMini3);
-document.getElementById("three").addEventListener("mouseout", mouseOutMini3);
+let three = document.getElementById("three");
+three.style.opacity="0";
+three.addEventListener("mouseover", mouseOverMini3);
+three.addEventListener("mouseout", mouseOutMini3);
 
 function mouseOverMini3() {
 document.getElementById("mini3").style.opacity = "1";
 }
-
 function mouseOutMini3() {
 document.getElementById("mini3").style.opacity = "0"
 }
 
-document.getElementById("four").addEventListener("mouseover", mouseOverMini4);
-document.getElementById("four").addEventListener("mouseout", mouseOutMini4);
+let four = document.getElementById("four");
+four.style.opacity="0";
+four.addEventListener("mouseover", mouseOverMini4);
+four.addEventListener("mouseout", mouseOutMini4);
 
 function mouseOverMini4() {
 document.getElementById("mini4").style.opacity = "1";
 }
-
 function mouseOutMini4() {
 document.getElementById("mini4").style.opacity = "0"
 }
 
-document.getElementById("five").addEventListener("mouseover", mouseOverMini5);
-document.getElementById("five").addEventListener("mouseout", mouseOutMini5);
+let five = document.getElementById("five");
+five.style.opacity="0";
+five.addEventListener("mouseover", mouseOverMini5);
+five.addEventListener("mouseout", mouseOutMini5);
 
 function mouseOverMini5() {
 document.getElementById("mini5").style.opacity = "1";
 }
-
 function mouseOutMini5() {
 document.getElementById("mini5").style.opacity = "0"
 }
 
-document.getElementById("six").addEventListener("mouseover", mouseOverMini6);
-document.getElementById("six").addEventListener("mouseout", mouseOutMini6);
+let six = document.getElementById("six");
+six.style.opacity="0";
+six.addEventListener("mouseover", mouseOverMini6);
+six.addEventListener("mouseout", mouseOutMini6);
 
 function mouseOverMini6() {
 document.getElementById("mini6").style.opacity = "1";
 }
-
 function mouseOutMini6() {
 document.getElementById("mini6").style.opacity = "0"
 }
 
-document.getElementById("seven").addEventListener("mouseover", mouseOverMini7);
-document.getElementById("seven").addEventListener("mouseout", mouseOutMini7);
+let seven = document.getElementById("seven");
+seven.style.opacity="0";
+seven.addEventListener("mouseover", mouseOverMini7);
+seven.addEventListener("mouseout", mouseOutMini7);
 
 function mouseOverMini7() {
 document.getElementById("mini7").style.opacity = "1";
 }
-
 function mouseOutMini7() {
 document.getElementById("mini7").style.opacity = "0"
 }
 
-
-document.getElementById("A").addEventListener("mouseover", mouseOverProjectA);
-document.getElementById("A").addEventListener("mouseout", mouseOutProjectA);
+let pA = document.getElementById("A");
+pA.style.opacity="0";
+pA.classList.add("boxes");
+pA.style.animationDelay="1s";
+pA.addEventListener("mouseover", mouseOverProjectA);
+pA.addEventListener("mouseout", mouseOutProjectA);
 
 function mouseOverProjectA() {
 document.getElementById("imga").style.opacity = "1"
 }
-
 function mouseOutProjectA() {
 document.getElementById("imga").style.opacity = "0"
 }
 
-document.getElementById("B").addEventListener("mouseover", mouseOverProjectB);
-document.getElementById("B").addEventListener("mouseout", mouseOutProjectB);
+let pB = document.getElementById("B");
+pB.style.opacity="0";
+pB.classList.add("boxes");
+pB.style.animationDelay="1.5s";
+pB.addEventListener("mouseover", mouseOverProjectB);
+pB.addEventListener("mouseout", mouseOutProjectB);
 
 function mouseOverProjectB() {
 document.getElementById("imgb").style.opacity = "1"
 }
-
 function mouseOutProjectB() {
 document.getElementById("imgb").style.opacity = "0"
 }
 
-document.getElementById("C").addEventListener("mouseover", mouseOverProjectC);
-document.getElementById("C").addEventListener("mouseout", mouseOutProjectC);
+let pC = document.getElementById("C");
+pC.style.opacity="0";
+pC.classList.add("boxes");
+pC.style.animationDelay="2s";
+pC.addEventListener("mouseover", mouseOverProjectC);
+pC.addEventListener("mouseout", mouseOutProjectC);
 
 function mouseOverProjectC() {
 document.getElementById("imgc").style.opacity = "1"
 }
-
 function mouseOutProjectC() {
 document.getElementById("imgc").style.opacity = "0"
 }
+
+let isInViewport = function (elem) {
+	let distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(one)) {
+    one.classList.add("boxes");
+    one.style.animationDelay="0.5s";
+    two.classList.add("boxes");
+    two.style.animationDelay="1s";
+    three.classList.add("boxes");
+    three.style.animationDelay="1.5s";
+    four.classList.add("boxes");
+    four.style.animationDelay="2s";
+    five.classList.add("boxes");
+    five.style.animationDelay="2.5s";
+    six.classList.add("boxes");
+    six.style.animationDelay="3s";
+    seven.classList.add("boxes");
+    seven.style.animationDelay="3.5s";
+	}
+}, false);
